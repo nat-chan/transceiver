c8d88e03105ae1c72fde316521cfa3e5a9733a63
nat-chan <ogino.natsuki.tm@alumni.tsukuba.ac.jp>
2024-04-21 04:56:29 +0900

    ComfyUI側でIMPORT通った

--- save_image_mem.py
+++ save_image_mem.py
@@ -1,23 +1,31 @@
 #!/usr/bin/env python3
+import comfy_annotations
+from comfy_annotations import ComfyFunc, ImageTensor, MaskTensor, NumberInput, Choice, StringInput
+from fs.memoryfs import MemoryFS
+mem_fs = MemoryFS()
+import numpy as np
 
-class SaveImageMem:
-    RETURN_TYPES = ("IMAGE", ) # 出力ノードのデータ型
-    RETURN_NAMES = ("image", ) # 出力ノード名
-    FUNCTION = "run" # ノード実行時に呼ばれる関数名
-    OUTPUT_NODE = False # 実行の進んでいく終端ノードとして扱うか
-    CATEGORY = "mathbbN" # 左クリックしたときに出てくるカテゴリ
-    @classmethod
-    def INPUT_TYPES(s):
-        return {
-            "required": {
-                "shellscript": ("STRING", {"multiline": True}),
-                "image": ("IMAGE", {}),
-            },
-        }
+NODE_CLASS_MAPPINGS = {}
+NODE_DISPLAY_NAME_MAPPINGS = {}
 
-    def run(self, shellscript, image):
-        print(shellscript)
-        return image
+@ComfyFunc(
+    category="Image",
+    display_name="SaveImageMem",
+    is_output_node=True,  # ComfyUI will always run this node
+    debug=True
+)
+def save_image_mem(
+        image: ImageTensor, 
+        filename_prefix: str = StringInput("ComfyUI", multiline=False),
+    ) -> None:
+    """
+    画像を入力としてうけつけ、指定されたパスのメモリファイルシステムに書きこむ
+    """.strip()
+    # torch のimageをcpu detachしてnumpy 
+    # 
+    np_array = image.detach().cpu().numpy()
+    with mem_fs.openbin(f"{filename_prefix}.npy", "w") as f:
+        np.save(f, np_array)
 
-NODE_CLASS_MAPPINGS = { "SaveImageMem": SaveImageMem }
-NODE_DISPLAY_NAME_MAPPINGS = { "SaveImageMem": "SaveImageMem" }
+NODE_CLASS_MAPPINGS.update(comfy_annotations.NODE_CLASS_MAPPINGS)
+NODE_DISPLAY_NAME_MAPPINGS.update(comfy_annotations.NODE_DISPLAY_NAME_MAPPINGS)
