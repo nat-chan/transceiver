14ce8a87e1d902603ae5c23f8faf58eade413a95
nat-chan <ogino.natsuki.tm@alumni.tsukuba.ac.jp>
2024-04-22 22:05:17 +0900

    pypiに登録

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # transceiver
 
+transceiver is a Python library that swiftly exchanges complex data structures such as numpy arrays and PIL images between processes, optimizing AI inference tasks that utilize ComfyUI.
+
 ## why?
 
 When processing a large number of requests, the SaveImage and LoadImage nodes may be IO-limited, and using shared memory improves performance by passing images only through memory access, not through IO.
--- src/transceiver/__init__.py
+++ src/transceiver/__init__.py
@@ -1,19 +1,19 @@
-#!/usr/bin/env python3
+import comfy_annotations
+
+import transceiver.save_image_mem
 
 NODE_CLASS_MAPPINGS = {}
 NODE_DISPLAY_NAME_MAPPINGS = {}
 
 # Import your module that defines nodes, so that ComfyFunc has a chance to process them.
-import save_image_mem
-
 # Actually add the @ComfyFunc nodes to what ComfyUI picks up.
-import comfy_annotations
-NODE_CLASS_MAPPINGS.update(comfy_annotations.NODE_CLASS_MAPPINGS) 
+
+NODE_CLASS_MAPPINGS.update(comfy_annotations.NODE_CLASS_MAPPINGS)
 NODE_DISPLAY_NAME_MAPPINGS.update(comfy_annotations.NODE_DISPLAY_NAME_MAPPINGS)
 
 # Set up any non-ComfyFunc node types as needed.
-# NODE_CLASS_MAPPINGS.update(example.example_nodes.NODE_CLASS_MAPPINGS) 
+# NODE_CLASS_MAPPINGS.update(example.example_nodes.NODE_CLASS_MAPPINGS)
 # NODE_DISPLAY_NAME_MAPPINGS.update(example.example_nodes.NODE_DISPLAY_NAME_MAPPINGS)
 
 # Export so that ComfyUI can pick them up.
-__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
+__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
