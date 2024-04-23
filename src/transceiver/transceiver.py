4f6b5461cbc5e43ec5db682d31586d92a09767ac
nat-chan <ogino.natsuki.tm@alumni.tsukuba.ac.jp>
2024-04-23 11:28:48 +0900

    list_managed_name,preをかいた

--- src/transceiver/transceiver.py
+++ src/transceiver/transceiver.py
@@ -1,6 +1,7 @@
 from multiprocessing import shared_memory  # noqa
 import ast  # noqa
 import numpy as np
+from pathlib import Path
 from typing import Literal
 
 
@@ -25,11 +26,25 @@ class Transceiver:
     def __init__(self):
         pass
 
+    def pre(name: str) -> str:
+        """管理を容易にするために共通するprefixを付ける"""
+        return f"transceiver-{name}"
+
     def read(self, name: str) -> np.ndarray:
+        name = self.pre(name)
         pass
 
     def write(self, name: str, data_format: data_format_t):
+        name = self.pre(name)
         pass
 
     def write_numpy(self, name: str, array: np.ndarray):
+        name = self.pre(name)
         pass
+
+    def list_managed_name(self) -> list[str]:
+        results = [
+            filepath.name.removeprefix("transceiver-")
+            for filepath in Path("/dev/shm").glob("transceiver-*")
+        ]
+        return results
